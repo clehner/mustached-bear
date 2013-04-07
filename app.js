8f52483a92ccc1117ae065816f37fe53caeff7c1
Charles Lehner <celehner1@gmail.com>
2013-04-07 05:16:33 -0400

    Update search and index routes

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/search', routes.search);
 app.get('/result', result.list);
 
 http.createServer(app).listen(app.get('port'), function(){
--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,7 @@
 exports.index = function(req, res){
   res.render('index', { title: 'mustached-bear' });
 };
+
+exports.search = function(req, res){
+  res.render('search', { title: 'mustached-bear' });
+};
